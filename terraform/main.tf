c4c7be4826301d5e4e29b1f0339c78450c739ad7
Ian Ellis <ellisiana@gmail.com>
2023-12-28 10:06:57 +0000

    added simple user_data script to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,11 @@ resource "aws_instance" "example_server" {
   ami           = data.aws_ami.linux.id
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello" > /tmp/world.txt
+              EOF
+
   tags = {
     Name = "ExampleServer"
   }
