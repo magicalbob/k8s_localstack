919e23e19d64efc1cecce0c76ba0e6f717c49dd9
Ian Ellis <ellisiana@gmail.com>
2023-12-29 20:24:08 +0000

    top level now go to drill down pages

--- localstack_ui/app.py
+++ localstack_ui/app.py
@@ -1,19 +1,22 @@
-#!/usr/bin/env python3
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
 
+# Read the config.json file
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
 @app.route('/')
 def landing_page():
-    # Read the configuration from config.json
-    with open('config.json', 'r') as config_file:
-        config_data = json.load(config_file)
+    return render_template('landing_page.html', services=config['top_level_services'])
 
-    # Extract the top-level services from the config
-    top_level_services = config_data.get('top_level_services', [])
-
-    return render_template('landing_page.html', top_level_services=top_level_services)
+@app.route('/<service_url>')
+def service_page(service_url):
+    service = next((s for s in config['top_level_services'] if s['service_url'] == service_url), None)
+    if service:
+        return f"<h1>{service['service_name']}</h1>"
+    return "Service not found"
 
 if __name__ == '__main__':
     app.run(debug=True)
