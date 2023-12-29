b27dabc92b1b666891be5b80fc764067e1595050
Ian Ellis <ellisiana@gmail.com>
2023-12-29 22:07:55 +0000

    got structure of app working

--- localstack_ui/app.py
+++ localstack_ui/app.py
@@ -24,6 +24,25 @@ def service(service_url):
         return render_template('service_page.html', service=service, sections=items)
     return "Service not found"
 
+@app.route('/<service_url>/<section_name>/<item_name>')
+def item_page(service_url, section_name, item_name):
+    service = next((s for s in config['top_level_services'] if s['service_url'] == service_url), None)
+    if service:
+        sections = config.get(service_url, {})
+        if section_name in sections:
+            items = sections[section_name]
+            for item in items:
+                if item['item'] == item_name:
+                    info_command = item.get('info_command')
+                    if info_command:
+                        # Execute the AWS command (info_command) here
+                        # You can use subprocess or any other method to run the command
+                        # and capture the output
+                        # For simplicity, let's assume info_command_output contains the command output
+                        info_command_output = "Sample output from AWS command"
+                        return render_template('item_page.html', service=service, section=section_name, item=item_name, output=info_command_output)
+    return "Service, section, or item not found"
+
 if __name__ == '__main__':
     app.run(debug=True)
 
