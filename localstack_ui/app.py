932e4a21bce325160299b147c59af19622854b03
Ian Ellis <ellisiana@gmail.com>
2023-12-29 21:41:40 +0000

    basic config.json driving menus

--- localstack_ui/app.py
+++ localstack_ui/app.py
@@ -12,11 +12,16 @@ with open('config.json', 'r') as config_file:
 def landing_page():
     return render_template('landing_page.html', services=config['top_level_services'])
 
-@app.route('/<service_url>')
-def service_page(service_url):
+@app.route('/service/<service_url>')
+def service(service_url):
     service = next((s for s in config['top_level_services'] if s['service_url'] == service_url), None)
     if service:
-        return f"<h1>{service['service_name']}</h1>"
+        sections = config.get(service_url, {})
+        items = []
+        for section_name, section_items in sections.items():
+            for item in section_items:
+                items.append(item)
+        return render_template('service_page.html', service=service, sections=items)
     return "Service not found"
 
 if __name__ == '__main__':
