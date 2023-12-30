985cfa5f8318cbb578f9e7f368d152d738a9c6ef
Ian Ellis <ellisiana@gmail.com>
2023-12-30 09:03:22 +0000

    Got json to html working

--- localstack_ui/app.py
+++ localstack_ui/app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template
 import json
 import subprocess
+from json2html import json2html
 
 app = Flask(__name__)
 
@@ -27,22 +28,26 @@ def service(service_url):
 
 @app.route('/<service_url>/<section_name>/<item_name>')
 def item_page(service_url, section_name, item_name):
-    service = next((s for s in config['top_level_services'] if s['service_url'] == service_url), None)
-    if service:
-        sections = config.get(service_url, {})
-        if section_name in sections:
-            items = sections[section_name]
-            for item in items:
-                if item['item'] == item_name:
-                    info_command = item.get('info_command')
-                    if info_command:
-                        try:
-                            # Execute the AWS command and capture its output
-                            info_command_output = subprocess.check_output(info_command, shell=True, text=True)
-                            return render_template('item_page.html', service=service, section=section_name, item=item_name, output=info_command_output)
-                        except subprocess.CalledProcessError as e:
-                            return f"Error executing AWS command: {str(e)}"
-    return "Service, section, or item not found"
+   service = next((s for s in config['top_level_services'] if s['service_url'] == service_url), None)
+   if service:
+       sections = config.get(service_url, {})
+       if section_name in sections:
+           items = sections[section_name]
+           for item in items:
+               if item['item'] == item_name:
+                   info_command = item.get('info_command')
+                   if info_command:
+                       try:
+                           # Execute the AWS command and capture its output as a string
+                           info_command_output = subprocess.check_output(info_command, shell=True, text=True)
+
+                           # Convert the JSON data to an HTML table
+                           json_data = json.loads(info_command_output)
+                           table_html = json2html.convert(json=json_data)
+
+                           return render_template('item_page.html', service=service, section=section_name, item=item_name, table_html=table_html)
+                       except subprocess.CalledProcessError as e:
+                           return f"Error executing AWS command: {str(e)}"
 
 if __name__ == '__main__':
     app.run(debug=True)
