d79d99f1d4d203d7d834359b765720bd91d1f07f
Ian Ellis <ellisiana@gmail.com>
2023-12-30 10:52:43 +0000

    drive from config.json

--- localstack_ui/app.py
+++ localstack_ui/app.py
@@ -19,35 +19,32 @@ def service(service_url):
     service = next((s for s in config['top_level_services'] if s['service_url'] == service_url), None)
     if service:
         sections = config.get(service_url, {})
-        items = []
-        for section_name, section_items in sections.items():
-            for item in section_items:
-                items.append(item)
-        return render_template('service_page.html', service=service, sections=items)
+        return render_template('service_page.html', service=service, sections=sections)
     return "Service not found"
 
 @app.route('/<service_url>/<section_name>/<item_name>')
 def item_page(service_url, section_name, item_name):
-   service = next((s for s in config['top_level_services'] if s['service_url'] == service_url), None)
-   if service:
-       sections = config.get(service_url, {})
-       if section_name in sections:
-           items = sections[section_name]
-           for item in items:
-               if item['item'] == item_name:
-                   info_command = item.get('info_command')
-                   if info_command:
-                       try:
-                           # Execute the AWS command and capture its output as a string
-                           info_command_output = subprocess.check_output(info_command, shell=True, text=True)
+    service = next((s for s in config['top_level_services'] if s['service_url'] == service_url), None)
+    if service:
+        sections = config.get(service_url, {})
+        if section_name in sections:
+            items = sections[section_name]
+            for item in items:
+                if item['item'] == item_name:
+                    info_command = item.get('info_command')
+                    if info_command:
+                        try:
+                            # Execute the AWS command and capture its output as a string
+                            info_command_output = subprocess.check_output(info_command, shell=True, text=True)
 
-                           # Convert the JSON data to an HTML table
-                           json_data = json.loads(info_command_output)
-                           table_html = json2html.convert(json=json_data)
+                            # Convert the JSON data to an HTML table
+                            json_data = json.loads(info_command_output)
+                            table_html = json2html.convert(json=json_data)
 
-                           return render_template('item_page.html', service=service, section=section_name, item=item_name, table_html=table_html)
-                       except subprocess.CalledProcessError as e:
-                           return f"Error executing AWS command: {str(e)}"
+                            return render_template('item_page.html', service=service, section=section_name, item=item_name, table_html=table_html)
+                        except subprocess.CalledProcessError as e:
+                            return f"Error executing AWS command: {str(e)}"
+    return "Service or section not found"
 
 if __name__ == '__main__':
     app.run(debug=True)
