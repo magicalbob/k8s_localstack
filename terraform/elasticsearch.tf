917ea46a4f871f66cf446474a2961ed02ca03994
Ian Ellis <ellisiana@gmail.com>
2024-01-02 15:26:47 +0000

    terraform fmt

--- terraform/elasticsearch.tf
+++ terraform/elasticsearch.tf
@@ -2,7 +2,7 @@ resource "aws_elasticsearch_domain" "example_domain" {
   domain_name           = "my-elastic-domain"
   elasticsearch_version = "7.10"
   cluster_config {
-    instance_type = "m4.large.elasticsearch"
+    instance_type  = "m4.large.elasticsearch"
     instance_count = 1
   }
   ebs_options {
@@ -13,12 +13,12 @@ resource "aws_elasticsearch_domain" "example_domain" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect    = "Allow",
+        Effect = "Allow",
         Principal = {
           Service = "es.amazonaws.com"
         },
-        Action    = "es:ESHttpPut",
-        Resource  = "arn:aws:es:${var.aws_region}:${var.aws_account_id}:domain/my-elastic-domain/*",
+        Action   = "es:ESHttpPut",
+        Resource = "arn:aws:es:${var.aws_region}:${var.aws_account_id}:domain/my-elastic-domain/*",
         Condition = {
           IpAddress : {
             "aws:SourceIp" : "192.168.0.10"
