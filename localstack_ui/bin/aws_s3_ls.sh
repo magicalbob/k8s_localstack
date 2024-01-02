ddae3dd468c87958f2c7390cb8a9d97b5c0ad916
Ian Ellis <ellisiana@gmail.com>
2024-01-02 12:15:12 +0000

    updated aws s3 ls to handle multiple buckets

--- localstack_ui/bin/aws_s3_ls.sh
+++ localstack_ui/bin/aws_s3_ls.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-aws s3 ls --output json | awk 'BEGIN {print "["} {print "{\"timestamp\": \"" $1 " " $2 "\", \"bucket\": \"" $3 "\"}"} END {print "]"}'
+# Run aws s3 ls and format the output as JSON
+aws s3 ls --output json | awk 'BEGIN {print "["} {if (NR > 1) {print ","} print "{\"timestamp\": \"" $1 " " $2 "\", \"bucket\": \"" $3 "\"}"} END {print "]"}'
+
