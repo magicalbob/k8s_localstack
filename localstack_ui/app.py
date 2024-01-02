b34139cc3804681b359847b059b214aa601ab907
Ian Ellis <ellisiana@gmail.com>
2024-01-02 20:29:40 +0000

    split massive if in item_page into two

--- localstack_ui/app.py
+++ localstack_ui/app.py
@@ -30,28 +30,33 @@ def item_page(service_url, section_name, item_name):
         if section_name in sections:
             items = sections[section_name]
             for item in items:
-                if item['item'] == item_name:
-                    info_command = item.get('info_command')
-                    if info_command:
-                        try:
-                            # Execute the AWS command and capture its output as a string
-                            info_command_output = subprocess.check_output(info_command, shell=True, text=True)
-
-                            # Check if the output is empty or not
-                            if not info_command_output.strip():
-                                return "No data available."
-
-                            # Convert the JSON data to an HTML table
-                            json_data = json.loads(info_command_output)
-                            table_html = json2html.convert(json=json_data)
-
-                            return render_template('item_page.html', service=service, section=section_name, item=item_name, table_html=table_html)
-                        except subprocess.CalledProcessError as e:
-                            return f"Error executing AWS command: {str(e)}"
-                        except json.JSONDecodeError as e:
-                            return f"Error decoding JSON data: {str(e)}"
+                result = process_item(service, item, section_name, item_name)
+                if result is not None:
+                    return result
     return "Service or section not found"
+
+def process_item(service, item, section_name, item_name):
+    if item['item'] == item_name:
+        info_command = item.get('info_command')
+        if info_command:
+            try:
+                # Execute the AWS command and capture its output as a string
+                info_command_output = subprocess.check_output(info_command, shell=True, text=True)
+
+                # Check if the output is empty or not
+                if not info_command_output.strip():
+                    print("DEBUG 5")
+                    return "No data available."
+
+                # Convert the JSON data to an HTML table
+                json_data = json.loads(info_command_output)
+                table_html = json2html.convert(json=json_data)
+
+                return render_template('item_page.html', service=service, section=section_name, item=item_name, table_html=table_html)
+            except subprocess.CalledProcessError as e:
+                return f"Error executing AWS command: {str(e)}"
+            except json.JSONDecodeError as e:
+                return f"Error decoding JSON data: {str(e)}"
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=8443)
-
